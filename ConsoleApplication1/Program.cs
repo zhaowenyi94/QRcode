0f64d00b152633910b02356ad5358c2b33dcba5f
Wenyi Zhao <zhaowenyi94@gmail.com>
2015-11-13 08:36:45 +0800

    findpattern processing added

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -45,7 +45,8 @@ namespace ConsoleApplication1
             srcBmp.UnlockBits(srcBmpData);
             return srcBmp;
         }
-        //Bitmap转灰度二值数组
+
+        //Bitmap转灰度二维数组
         static byte[,] GetGrayArray2D(Bitmap srcBmp)
         {
             Rectangle rect = new Rectangle(0, 0, srcBmp.Width, srcBmp.Height);
@@ -89,10 +90,14 @@ namespace ConsoleApplication1
 
             return grayValues;
         }
+
+        //判断非零项
         static Boolean NonZero(Int32 value)
         {
             return (value != 0) ? true : false;
         }
+        
+        //大通发求滤波阀值
         static Int32 OtsuThreshold(Byte[,] grayarray)
         {
             Int32[] Histogram = new int[256];
@@ -131,6 +136,8 @@ namespace ConsoleApplication1
             }
             return Threshold;
         }
+
+        //带阀值全局二值化
         static void Thresholding(Bitmap rsc, Int32 threshold)
         {
             for (int i = 0; i < rsc.Width; i++)
@@ -143,7 +150,208 @@ namespace ConsoleApplication1
                 }
             }
         }
+        //找到坐标点
+        static void Processing(Bitmap curBitmap)
+        {
+            if (curBitmap != null)
+            {
+                double[,] process = new double[curBitmap.Height, curBitmap.Width];
+                for (int j = 0; j < curBitmap.Height; j++)
+                {
+                    int i = 0;
+                    int counter = -1;
+                    int sum = 1;
+                    while (i < curBitmap.Width)
+                    {
+                        while (i < curBitmap.Width - 1 && (curBitmap.GetPixel(i, j)).R == (curBitmap.GetPixel(i + 1, j)).R)
+                        {
+                            sum = sum + 1;
+                            i = i + 1;
+                        }
+                        counter = counter + 1;
+                        process[j, counter] = sum;
+                        sum = 1;
+                        i = i + 1;
+                    }
+                }
 
+                //得到了process
+                double[,] suitratio = new double[1000, 1000];
+                int sum1 = 0;//sum1是之前的横坐标数目
+                for (int row = 0; row < curBitmap.Height; row++)
+                {
+                    for (int col = 0; col < curBitmap.Width - 4; col++)
+                    {
+                        double[] select = new double[5];
+                        for (int i = 0; i < 5; i++)
+                        {
+                            select[i] = process[row, col + i];
+                        }
+
+                        double max = select.Max();
+                        double selectsum = select.Sum();
+                        double baselen = (selectsum - max) / 4;
+                        if (baselen != 0)
+                        {
+                            if (select[0] >= baselen * 0.5 && select[0] <= baselen * 1.5 && select[1] >= baselen * 0.5 && select[1] <= baselen * 1.5 && select[2] >= baselen * 2.1 && select[2] <= baselen * 3.9 && select[3] >= baselen * 0.5 && select[3] <= baselen * 1.5 && select[4] >= baselen * 0.5 && select[4] <= baselen * 1.5)
+                            {
+                                for (int k = 0; k < col + 2; k++)
+                                    sum1 += (int)process[row, k];
+                                if (select[2] % 2 == 1)
+                                {
+                                    sum1 = sum1 + (int)(select[2] / 2) + 1;
+                                    if (curBitmap.GetPixel(sum1, row).R == 0)
+                                    {
+                                        suitratio[row, sum1] = 1;
+                                    }
+                                }
+                                else
+                                {
+                                    sum1 = sum1 + (int)(select[2] / 2);
+                                    if (curBitmap.GetPixel(sum1, row).R == 0)
+                                    {
+                                        suitratio[row, sum1] = 1;
+                                    }
+                                    sum1 = sum1 + 1;
+                                    if (curBitmap.GetPixel(sum1, row).R == 0)
+                                    {
+                                        suitratio[row, sum1] = 1;
+                                    }
+                                }
+                            }
+                        }
+                        sum1 = 0;
+                    }
+                }//end of  process过后的 行处理
+
+
+                double[,] process1 = new double[1000, 1000];
+                for (int j = 0; j < curBitmap.Width; j++)
+                {
+                    int i = 0;
+                    int counter = -1;
+                    int sum = 1;
+                    while (i < curBitmap.Height)
+                    {
+                        while ((i < curBitmap.Height - 1) && (curBitmap.GetPixel(j, i)).R == (curBitmap.GetPixel(j, i + 1)).R)
+                        {
+                            sum = sum + 1;
+                            i = i + 1;
+                        }
+                        counter = counter + 1;
+                        process1[counter, j] = sum;
+                        sum = 1;
+                        i = i + 1;
+                    }
+                }
+
+                //得到了process1
+                sum1 = 0;//sum1是之前的横坐标数目
+                for (int col = 0; col < curBitmap.Width; col++)
+                {
+                    for (int row = 0; row < curBitmap.Height - 4; row++)
+                    {
+                        double[] select = new double[5];
+                        for (int i = 0; i < 5; i++)
+                        {
+                            select[i] = process1[row + i, col];
+                        }
+                        double max = select.Max();
+                        double selectsum = select.Sum();
+                        double baselen = (selectsum - max) / 4;
+                        if (baselen != 0)
+                        {
+                            if (select[0] >= baselen * 0.5 && select[0] <= baselen * 1.5 && select[1] >= baselen * 0.5 && select[1] <= baselen * 1.5 && select[2] >= baselen * 2.1 && select[2] <= baselen * 3.9 && select[3] >= baselen * 0.5 && select[3] <= baselen * 1.5 && select[4] >= baselen * 0.5 && select[4] <= baselen * 1.5)
+                            {
+                                for (int k = 0; k < row + 2; k++)
+                                    sum1 = sum1 + (int)process1[k, col];
+                                if (select[2] % 2 == 1)
+                                {
+                                    sum1 = sum1 + (int)(select[2] / 2) + 1;
+                                    if (curBitmap.GetPixel(col, sum1).R == 0)
+                                    {
+                                        suitratio[sum1, col] = suitratio[sum1, col] + 1;
+                                    }
+                                }
+                                else
+                                {
+                                    sum1 = sum1 + (int)(select[2] / 2);
+                                    if (curBitmap.GetPixel(col, sum1).R == 0)
+                                    {
+                                        suitratio[sum1, col] = suitratio[sum1, col] + 1;
+                                    }
+                                    sum1 = sum1 + 1;
+                                    if (curBitmap.GetPixel(col, sum1).R == 0)
+                                    {
+                                        suitratio[sum1, col] = suitratio[sum1, col] + 1;
+                                    }
+                                }
+                            }
+                        }
+                        sum1 = 0;
+                    }
+                }//end of  process过后的 行处理
+                double co_x;
+                double co_y;
+                int count = -1;
+                double[,] point = new double[3, 2];//之后单独写宏定义，在检测时输入有几个被监测的二维码
+                for (int row = 0; row < curBitmap.Height; row++)
+                {
+                    for (int col = 0; col < curBitmap.Width; col++)
+                    {
+                        if (suitratio[row, col] == 2)
+                        {
+                            if (suitratio[row + 1, col] == 2 && suitratio[row, col + 1] == 2)
+                            {
+                                suitratio[row + 1, col] = 0;
+                                suitratio[row, col + 1] = 0;
+                                suitratio[row + 1, col + 1] = 0;
+                                co_x = col + 0.5;
+                                co_y = row + 0.5;
+                            }
+                            else if (suitratio[row + 1, col] == 2)
+                            {
+                                suitratio[row + 1, col] = 0;
+                                co_x = col;
+                                co_y = row + 0.5;
+                            }
+                            else if (suitratio[row, col + 1] == 2)
+                            {
+                                suitratio[row, col + 1] = 0;
+                                suitratio[row + 1, col + 1] = 0;
+                                co_x = col + 0.5;
+                                co_y = row;
+                            }
+                            else { co_x = col; co_y = row; }
+                            count = count + 1;
+                            point[count, 0] = co_x;
+                            point[count, 1] = co_y;
+                            Graphics g;
+                            g = Graphics.FromImage(curBitmap); //创建画板
+                            Rectangle rg = new Rectangle(col - 5, row - 5, 10, 10); //填充区域
+                            g.FillRectangle(new SolidBrush(Color.Red), rg); //填充黑色
+                        }
+                    }
+
+                }
+                String str = "coordinate:\n";
+                for (int row = 0; row < 3; row++)
+                    for (int col = 0; col < 2; col++)
+                    {
+                        String str1 = point[row, col].ToString();
+                        if (col == 0)
+                            str = str + "\n" + str1;
+                        else
+                            str = str + "  " + str1;
+                    }
+                Console.WriteLine(str);
+            }
+        }
+
+
+
+
+        //主函数
         static void Main(string[] args)
         {
             Bitmap map = new Bitmap("D:/Documents/Projects/C#/ConsoleApplication1/ConsoleApplication1/test.png");
@@ -151,6 +359,7 @@ namespace ConsoleApplication1
             byte[,] GrayArray = GetGrayArray2D(map);
             Int32 Threshold = OtsuThreshold(GrayArray);
             Thresholding(map,Threshold);
+            Processing(map);
             map.Save("test.jpg", ImageFormat.Jpeg);
         }
     }
